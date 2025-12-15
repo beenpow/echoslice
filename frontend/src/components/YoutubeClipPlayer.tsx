6555a6e3105768d701d3e2d8e1a85f832ef880ba
beenpow <57864041+beenpow@users.noreply.github.com>
2025-12-14 21:47:40 -0800

    front: step1: Adding a DOM to refer youtube clip later.

--- frontend/src/components/YoutubeClipPlayer.tsx
+++ frontend/src/components/YoutubeClipPlayer.tsx
@@ -7,9 +7,22 @@ type Props = {
 };
 
 export default function YoutubeClipPlayer({ videoId, startSec, endSec }: Props) {
+    // (1) ref for the DOM
+    const containerRef = useRef<HTMLDivElement>(null);
+
     return (
         <div>
             <h2>YoutubeClipPayer</h2>
+            {/* Youtube Player position */}
+            <div
+                ref={containerRef}
+                style={{
+                    width: 640,
+                    height: 360,
+                    background: "#222",
+                }}
+            />
+
             <div>VideoId: {videoId}</div>
             <div>
                 start: {startSec}s, end: {endSec}s
