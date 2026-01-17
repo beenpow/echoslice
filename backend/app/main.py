6e7c8b0d8a436c06d79dcfbfcad6f6ec4cc97390
beenpow <57864041+beenpow@users.noreply.github.com>
2026-01-16 20:03:50 -0800

    back: step21: Setting up for deployment.

--- backend/app/main.py
+++ backend/app/main.py
@@ -45,8 +45,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://echoslicefront.vercel.app",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
